809a111bf9b31ef6b03a203e36456dc07f68b1f5
Ali Ashik <ali.ashik@valtech.co.uk>
2018-09-10 13:08:32 +0100

    BBB-399 added acceptance test for 404 and refactored template to display stack trace

--- src/main/java/uk/gov/dft/bluebadge/webapp/citizen/controllers/errorhandler/ErrorController.java
+++ src/main/java/uk/gov/dft/bluebadge/webapp/citizen/controllers/errorhandler/ErrorController.java
@@ -9,19 +9,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class ErrorController {
 
-    public static final String URL_500_ERROR = "/something-went-wrong";
-    public static final String TEMPLATE_500_ERROR = "error/500";
+  public static final String URL_500_ERROR = "/something-went-wrong";
+  public static final String TEMPLATE_500_ERROR = "error/500";
 
-    public static final String URL_400_ERROR = "/page-not-found";
-    public static final String TEMPLATE_400_ERROR = "error/400";
-
-    @GetMapping(URL_500_ERROR)
-    public String show500(Model model) {
-        return TEMPLATE_500_ERROR;
-    }
-
-    @GetMapping(URL_400_ERROR)
-    public String show400() {
-        return TEMPLATE_400_ERROR;
-    }
+  @GetMapping(URL_500_ERROR)
+  public String show500(Model model) {
+    return TEMPLATE_500_ERROR;
+  }
 }
--- src/main/java/uk/gov/dft/bluebadge/webapp/citizen/controllers/errorhandler/ErrorControllerAdvice.java
+++ src/main/java/uk/gov/dft/bluebadge/webapp/citizen/controllers/errorhandler/ErrorControllerAdvice.java
@@ -1,72 +1,44 @@
 package uk.gov.dft.bluebadge.webapp.citizen.controllers.errorhandler;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import javax.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import uk.gov.dft.bluebadge.webapp.citizen.client.common.ClientApiException;
 
-import javax.servlet.http.HttpServletRequest;
-
 @Slf4j
 @ControllerAdvice
 public class ErrorControllerAdvice {
 
-  // Dev
-  //@Autowired ObjectMapper objectMapper;
+  @Autowired ObjectMapper objectMapper;
 
   @ExceptionHandler(Exception.class)
-  public String handleException(Exception ex, HttpServletRequest req, RedirectAttributes redirectAttributes) {
-    //String message = ex.getClass().getName();
-    //logException(message, ex);
-    //redirectAttributes.addFlashAttribute("exception", ex);
+  public String handleException(
+      Exception ex, HttpServletRequest req, RedirectAttributes redirectAttributes) {
     log.error("Request: {} raised {}.", req.getRequestURL(), ex.toString(), ex);
-
+    redirectAttributes.addFlashAttribute("exception", ex);
     return "redirect:" + ErrorController.URL_500_ERROR;
   }
 
   @ExceptionHandler(ClientApiException.class)
-  public String handleHttpException(ClientApiException ex, RedirectAttributes redirectAttributes) {
-    //String message = ex.getClass().getName();
-    //logException(message, ex);
-    //redirectAttributes.addFlashAttribute("exception", ex);
-
-    return "redirect:" + ErrorController.URL_500_ERROR;
-  }
-
-  private void logException(String title, Exception ex) {
-    log.debug(title + " exception, exception [()], details [()]", ex.getMessage(), ex);
-  }
-
-  // Dev
-  /*@Profile("dev")
-  @ExceptionHandler
-  public ModelAndView handleError(HttpServletRequest req, ClientApiException ex) {
+  public String handleHttpException(
+      ClientApiException ex, RedirectAttributes redirectAttributes, HttpServletRequest req) {
     log.error("Request: {} raised {}.", req.getRequestURL(), ex.toString(), ex);
 
-    ModelAndView mav = new ModelAndView();
     try {
       String commonResponse =
           objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(ex.getCommonResponse());
       log.warn("Exception common response:{}", commonResponse);
-      mav.addObject("commonResponse", commonResponse);
+      redirectAttributes.addFlashAttribute("commonResponse", commonResponse);
     } catch (JsonProcessingException e) {
       log.warn("Failed to convert common response from exception.", e);
     }
-    mav.addObject("exception", ex);
-    mav.addObject("url", req.getRequestURL());
-    mav.setViewName("dev-error");
-    return mav;
+
+    redirectAttributes.addFlashAttribute("exception", ex);
+    return "redirect:" + ErrorController.URL_500_ERROR;
   }
-
-  @ExceptionHandler
-  public ModelAndView handleError(HttpServletRequest req, Exception ex) {
-    log.error("Request: {} raised {}.", req.getRequestURL(), ex.toString(), ex);
-
-    ModelAndView mav = new ModelAndView();
-    mav.addObject("exception", ex);
-    mav.addObject("url", req.getRequestURL());
-    mav.setViewName("dev-error");
-    return mav;
-  }*/
 }
