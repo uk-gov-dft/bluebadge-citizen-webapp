ce8f8645e8b2221ce8fe78e8e73c80f3f35efbef
Stephen Elliott <stephen@bealine.co.uk>
2018-09-20 09:15:04 +0100

    BBB-698 Add controller tests

---
+++ src/test/java/uk/gov/dft/bluebadge/webapp/citizen/controllers/MayBeEligibleControllerTest.java
@@ -0,0 +1,64 @@
+package uk.gov.dft.bluebadge.webapp.citizen.controllers;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import org.hamcrest.Matchers;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import uk.gov.dft.bluebadge.webapp.citizen.StandaloneMvcTestViewResolver;
+import uk.gov.dft.bluebadge.webapp.citizen.controllers.journey.RouteMaster;
+import uk.gov.dft.bluebadge.webapp.citizen.controllers.journey.StepDefinition;
+import uk.gov.dft.bluebadge.webapp.citizen.model.Journey;
+import uk.gov.dft.bluebadge.webapp.citizen.model.form.ApplicantForm;
+
+public class MayBeEligibleControllerTest {
+
+  private MockMvc mockMvc;
+  private MayBeEligibleController controller;
+
+  @Mock
+  private RouteMaster mockRouteMaster;
+  private Journey journey;
+
+  @Before
+  public void setup() {
+    MockitoAnnotations.initMocks(this);
+    controller = new MayBeEligibleController(mockRouteMaster);
+    mockMvc =
+        MockMvcBuilders.standaloneSetup(controller)
+            .setViewResolvers(new StandaloneMvcTestViewResolver())
+            .build();
+    journey = new Journey();
+    journey.setApplicantForm(ApplicantForm.builder().build());
+  }
+
+  @Test
+  public void show_ShouldDisplayEligibleTemplate() throws Exception {
+
+    mockMvc
+        .perform(get("/may-be-eligible").sessionAttr("JOURNEY", journey))
+        .andExpect(status().isOk())
+        .andExpect(view().name("may-be-eligible"))
+        .andExpect(model().attribute("formRequest", Matchers.nullValue()));
+  }
+
+  @Test
+  public void startApplication_ShouldRedirectToNextPage() throws Exception {
+
+    when(mockRouteMaster.redirectToOnSuccess(StepDefinition.MAY_BE_ELIGIBLE)).thenReturn("redirect:/theNextPage");
+
+    mockMvc
+        .perform(get("/may-be-eligible/start"))
+        .andExpect(status().isFound())
+        .andExpect(redirectedUrl("/theNextPage"));
+  }
+}
