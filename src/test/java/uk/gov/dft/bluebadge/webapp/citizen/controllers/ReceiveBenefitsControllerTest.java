038df0d7205caff14dba88dfa9d9ad70ae12598c
Stephen Elliott <stephen@bealine.co.uk>
2018-09-24 12:24:24 +0100

    BBB-698 Correct test - redirect after validation failure.

--- src/test/java/uk/gov/dft/bluebadge/webapp/citizen/controllers/ReceiveBenefitsControllerTest.java
+++ src/test/java/uk/gov/dft/bluebadge/webapp/citizen/controllers/ReceiveBenefitsControllerTest.java
@@ -4,10 +4,12 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.flash;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static uk.gov.dft.bluebadge.webapp.citizen.model.form.ApplicantType.YOURSELF;
 
 import org.hamcrest.Matchers;
 import org.junit.Before;
@@ -39,7 +41,10 @@ public class ReceiveBenefitsControllerTest {
             .setViewResolvers(new StandaloneMvcTestViewResolver())
             .build();
     journey = new Journey();
-    journey.setApplicantForm(ApplicantForm.builder().build());
+    journey.setApplicantForm(ApplicantForm.builder().applicantType(YOURSELF.name()).build());
+    when(mockRouteMaster.backToCompletedPrevious()).thenReturn("backToStart");
+    when(mockRouteMaster.redirectToOnBindingError(any(), any(), any(), any())).thenReturn("redirect:/someValidationError");
+
   }
 
   @Test
@@ -82,8 +87,7 @@ public class ReceiveBenefitsControllerTest {
   public void submit_whenMissingBenefitsAnswer_ThenShouldHaveValidationError() throws Exception {
     mockMvc
         .perform(post("/benefits").sessionAttr("JOURNEY", journey))
-        .andExpect(status().isOk())
-        .andExpect(view().name("receive-benefits"))
-        .andExpect(model().attributeHasFieldErrorCode("formRequest", "benefitType", "NotNull"));
+        .andExpect(status().isFound())
+        .andExpect(redirectedUrl("/someValidationError"));
   }
 }
