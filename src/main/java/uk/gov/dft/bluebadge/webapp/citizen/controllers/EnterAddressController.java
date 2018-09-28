ca56fd28c7da02631bffeb2dba7ed4133b67e66a
Ali Ashik <ali.ashik@valtech.co.uk>
2018-09-28 15:52:31 +0100

    BBB-709 fixed address field widths along with some accessibility improvements

--- src/main/java/uk/gov/dft/bluebadge/webapp/citizen/controllers/EnterAddressController.java
+++ src/main/java/uk/gov/dft/bluebadge/webapp/citizen/controllers/EnterAddressController.java
@@ -3,11 +3,11 @@ package uk.gov.dft.bluebadge.webapp.citizen.controllers;
 import static uk.gov.dft.bluebadge.webapp.citizen.model.Journey.FORM_REQUEST;
 import static uk.gov.dft.bluebadge.webapp.citizen.model.Journey.JOURNEY_SESSION_KEY;
 
+import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,8 +18,6 @@ import uk.gov.dft.bluebadge.webapp.citizen.controllers.journey.RouteMaster;
 import uk.gov.dft.bluebadge.webapp.citizen.controllers.journey.StepDefinition;
 import uk.gov.dft.bluebadge.webapp.citizen.model.Journey;
 import uk.gov.dft.bluebadge.webapp.citizen.model.form.EnterAddressForm;
-
-import javax.validation.Valid;
 
 @Controller
 @RequestMapping(Mappings.URL_ENTER_ADDRESS)
