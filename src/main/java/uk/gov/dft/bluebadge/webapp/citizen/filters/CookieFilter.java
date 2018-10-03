22629beade7886188c729bae19a745d16843c176
Ali Ashik <ali.ashik@valtech.co.uk>
2018-10-03 14:15:28 +0100

    BBB-844 added links to new feedback page. Fixed footer to stretch to the bottom of the screen

--- src/main/java/uk/gov/dft/bluebadge/webapp/citizen/filters/CookieFilter.java
+++ src/main/java/uk/gov/dft/bluebadge/webapp/citizen/filters/CookieFilter.java
@@ -1,10 +1,6 @@
-package uk.gov.dft.bluebadge.webapp.citizen;
+package uk.gov.dft.bluebadge.webapp.citizen.filters;
 
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.Objects;
-import java.util.Optional;
-import java.util.stream.Stream;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
@@ -16,14 +12,14 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
+import uk.gov.dft.bluebadge.webapp.citizen.utilities.CookieUtils;
 
 @Component
 @Order(999)
 public class CookieFilter implements Filter {
 
-  public static final String COOKIE_BANNER_KEY = "cookie_banner_seen";
   public static final String COOKIE_BANNER_VALUE = "yes";
-  public static final int SECONDS_IN_MONTH = 2628000;
+  public static final int SECONDS_IN_MONTH = 2592000;
 
   @Override
   public void init(FilterConfig filterConfig) throws ServletException {}
@@ -35,12 +31,8 @@ public class CookieFilter implements Filter {
     HttpServletRequest req = (HttpServletRequest) request;
     HttpServletResponse res = (HttpServletResponse) response;
 
-    Cookie[] cookies = req.getCookies();
-    Stream<Cookie> stream = Objects.nonNull(cookies) ? Arrays.stream(cookies) : Stream.empty();
-    Optional<Cookie> cookie = stream.filter(c -> COOKIE_BANNER_KEY.equals(c.getName())).findFirst();
-
-    if (!cookie.isPresent()) {
-      Cookie newCookie = new Cookie(COOKIE_BANNER_KEY, COOKIE_BANNER_VALUE);
+    if (!CookieUtils.isCookieBannerSet(req)) {
+      Cookie newCookie = new Cookie(CookieUtils.COOKIE_BANNER_KEY, COOKIE_BANNER_VALUE);
       newCookie.setMaxAge(SECONDS_IN_MONTH);
       res.addCookie(newCookie);
     }
