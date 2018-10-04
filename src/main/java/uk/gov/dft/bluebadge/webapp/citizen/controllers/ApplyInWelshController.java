9ebcb3fbc8c214ec9c97fde0b5b8f24f7266534c
Miguel Gil <mgilgar@gmail.com>
2018-10-04 14:50:39 +0100

    [BBB-817-signpost-welsh-language-applicants-to-northgate-journey] Controller unit test.

--- src/main/java/uk/gov/dft/bluebadge/webapp/citizen/controllers/ApplyInWelshController.java
+++ src/main/java/uk/gov/dft/bluebadge/webapp/citizen/controllers/ApplyInWelshController.java
@@ -17,48 +17,6 @@ public class ApplyInWelshController {
 
   @GetMapping
   public String show() {
-    /*
-        if (!journey.isValidState(getStepDefinition())) {
-          return routeMaster.backToCompletedPrevious();
-        }
-
-        if (!model.containsAttribute(FORM_REQUEST) && null != journey.getGenderForm()) {
-          model.addAttribute(FORM_REQUEST, journey.getGenderForm());
-        }
-
-        if (!model.containsAttribute(FORM_REQUEST)) {
-          model.addAttribute(FORM_REQUEST, GenderForm.builder().build());
-        }
-    */
-    //  setupModel(model, journey);
-
     return TEMPLATE;
   }
-  /*
-  @PostMapping
-  public String submit(
-      @ModelAttribute(JOURNEY_SESSION_KEY) Journey journey,
-      @Valid @ModelAttribute(FORM_REQUEST) GenderForm formRequest,
-      BindingResult bindingResult,
-      RedirectAttributes attr) {
-
-    if (bindingResult.hasErrors()) {
-      return routeMaster.redirectToOnBindingError(this, formRequest, bindingResult, attr);
-    }
-
-    journey.setGenderForm(formRequest);
-
-    return routeMaster.redirectToOnSuccess(formRequest, journey);
-  }*/
-  /*
-    @Override
-    public StepDefinition getStepDefinition() {
-      return StepDefinition.GENDER;
-    }
-  */
-  /*
-  private void setupModel(Model model, Journey journey) {
-    model.addAttribute("options", getOptions(journey));
-  }*/
-
 }
