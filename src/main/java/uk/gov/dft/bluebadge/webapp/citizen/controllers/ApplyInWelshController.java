0926ed61d9b451d370439a234f6c8ac98d424304
Miguel Gil <mgilgar@gmail.com>
2018-10-08 11:03:54 +0100

    [BBB-817-signpost-welsh-language-applicants-to-northgate-journey] Acceptance Tests + sonar qube comments.

--- src/main/java/uk/gov/dft/bluebadge/webapp/citizen/controllers/ApplyInWelshController.java
+++ src/main/java/uk/gov/dft/bluebadge/webapp/citizen/controllers/ApplyInWelshController.java
@@ -1,6 +1,5 @@
 package uk.gov.dft.bluebadge.webapp.citizen.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,9 +10,6 @@ import uk.gov.dft.bluebadge.webapp.citizen.controllers.journey.Mappings;
 public class ApplyInWelshController {
 
   private static final String TEMPLATE = "apply-in-welsh";
-
-  @Autowired
-  public ApplyInWelshController() {}
 
   @GetMapping
   public String show() {
