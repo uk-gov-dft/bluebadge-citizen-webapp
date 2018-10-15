2a486bb7f554d8d7c2cd42e3b700659f220af042
Robert Smallwood <38761367+robsvaltech@users.noreply.github.com>
2018-10-15 16:28:17 +0100

    Update env-feature.sh

--- acceptance-tests/env-feature.sh
+++ acceptance-tests/env-feature.sh
@@ -3,10 +3,3 @@
 # Override the version with the branch name
 export CA_VERSION=$(cat VERSION-computed)
 
-export AP_VERSION=0.7.0-SNAPSHOT
-export AZ_VERSION=0.10.0-SNAPSHOT
-export LA_VERSION=0.11.0-SNAPSHOT
-export UM_VERSION=0.11.0-SNAPSHOT
-export MG_VERSION=0.7.0-SNAPSHOT
-export RD_VERSION=0.9.0-SNAPSHOT
-export BB_VERSION=0.8.0-SNAPSHOT
