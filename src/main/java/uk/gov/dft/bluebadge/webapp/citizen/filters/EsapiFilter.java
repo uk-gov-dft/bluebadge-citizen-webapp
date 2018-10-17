5edfa7820d1a94b5a53d5b8c33257cd0047cabcd
Robert Smallwood <rob.smallwood@valtech.co.uk>
2018-10-17 15:24:49 +0100

    BBB-935 - Added ESAPI filter

---
+++ src/main/java/uk/gov/dft/bluebadge/webapp/citizen/filters/EsapiFilter.java
@@ -0,0 +1,65 @@
+package uk.gov.dft.bluebadge.webapp.citizen.filters;
+
+import lombok.extern.slf4j.Slf4j;
+import org.owasp.esapi.filters.SecurityWrapperRequest;
+import org.owasp.esapi.filters.SecurityWrapperResponse;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+@Order(500)
+@Slf4j
+public class EsapiFilter extends OncePerRequestFilter {
+
+  /**
+   * Wraps the request/response using a secure implementation that sanitises the headers/cookies/parameters etc..
+   * @param request
+   * @param response
+   * @param filterChain
+   * @throws ServletException
+   * @throws IOException
+   */
+  @Override
+  protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+    if (isAsyncDispatch(request)) {
+      filterChain.doFilter(request, response);
+    } else {
+      doFilterWrapped(wrapRequest(request), wrapResponse(response), filterChain);
+    }
+
+  }
+
+  protected void doFilterWrapped(SecurityWrapperRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+      try {
+        filterChain.doFilter(request, response);
+      }
+      catch (Exception e) {
+        log.error("Unable to complete filter chain:", e);
+      }
+  }
+
+  private SecurityWrapperRequest wrapRequest(HttpServletRequest request) {
+    if (request instanceof SecurityWrapperRequest) {
+      return (SecurityWrapperRequest) request;
+    } else {
+      return new SecurityWrapperRequest(request);
+    }
+  }
+
+  private SecurityWrapperResponse wrapResponse(HttpServletResponse response) {
+    if (response instanceof SecurityWrapperRequest) {
+      return (SecurityWrapperResponse) response;
+    } else {
+      return new SecurityWrapperResponse(response);
+    }
+  }
+
+}
