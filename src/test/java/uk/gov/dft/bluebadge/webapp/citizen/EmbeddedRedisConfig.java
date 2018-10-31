9814ae113c7fb9f8c14c2402d2d89eb59346fb05
Stephen Elliott <stephen@bealine.co.uk>
2018-10-31 10:52:15 +0000

    BBB-437 Add embedded Redis for unit testing

---
+++ src/test/java/uk/gov/dft/bluebadge/webapp/citizen/EmbeddedRedisConfig.java
@@ -0,0 +1,27 @@
+package uk.gov.dft.bluebadge.webapp.citizen;
+
+import java.io.IOException;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import redis.embedded.RedisServer;
+
+@Component
+public class EmbeddedRedisConfig {
+  @Value("${spring.redis.port}")
+  private int redisPort;
+
+  private RedisServer redisServer;
+
+  @PostConstruct
+  public void startRedis() throws IOException {
+    redisServer = new RedisServer(redisPort);
+    redisServer.start();
+  }
+
+  @PreDestroy
+  public void stopRedis() {
+    redisServer.stop();
+  }
+}
