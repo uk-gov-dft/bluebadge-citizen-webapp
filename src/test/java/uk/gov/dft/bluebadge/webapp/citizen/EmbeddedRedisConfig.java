ccecb0f4ca52e52d70ee2f5f08f03a7259f96057
Stephen Elliott <stephen@bealine.co.uk>
2018-10-31 11:59:02 +0000

    BBB-437 Add embedded Redis for unit testing - add logging when the server does not start

--- src/test/java/uk/gov/dft/bluebadge/webapp/citizen/EmbeddedRedisConfig.java
+++ src/test/java/uk/gov/dft/bluebadge/webapp/citizen/EmbeddedRedisConfig.java
@@ -3,11 +3,14 @@ package uk.gov.dft.bluebadge.webapp.citizen;
 import java.io.IOException;
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import redis.embedded.RedisServer;
+import redis.embedded.exceptions.EmbeddedRedisException;
 
 @Component
+@Slf4j
 public class EmbeddedRedisConfig {
   @Value("${spring.redis.port}")
   private int redisPort;
@@ -16,8 +19,14 @@ public class EmbeddedRedisConfig {
 
   @PostConstruct
   public void startRedis() throws IOException {
+    log.info("Starting embedded redis on port:{}", redisPort);
     redisServer = new RedisServer(redisPort);
-    redisServer.start();
+    try {
+      redisServer.start();
+    } catch (EmbeddedRedisException e) {
+      log.error("Failed to start embedded redis.", e);
+      throw e;
+    }
   }
 
   @PreDestroy
