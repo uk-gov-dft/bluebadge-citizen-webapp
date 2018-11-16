bbb7a5001480ac9acdf84902c57457c377801649
xytrams <yuliya.polyeno@gmail.com>
2018-11-16 09:58:33 +0000

    BBB-761 added test for converter

---
+++ src/test/java/uk/gov/dft/bluebadge/webapp/citizen/appbuilder/MedicalEquipmentConverterTest.java
@@ -0,0 +1,26 @@
+package uk.gov.dft.bluebadge.webapp.citizen.appbuilder;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static uk.gov.dft.bluebadge.webapp.citizen.client.applicationmanagement.model.EligibilityCodeField.CHILDBULK;
+
+import org.junit.Test;
+
+import static uk.gov.dft.bluebadge.webapp.citizen.client.applicationmanagement.model.BulkyMedicalEquipmentTypeCodeField.PUMP;
+import static uk.gov.dft.bluebadge.webapp.citizen.client.applicationmanagement.model.BulkyMedicalEquipmentTypeCodeField.OTHER;
+import uk.gov.dft.bluebadge.webapp.citizen.client.applicationmanagement.model.ChildUnder3;
+import uk.gov.dft.bluebadge.webapp.citizen.fixture.JourneyBuilder;
+
+public class MedicalEquipmentConverterTest {
+
+	  @Test
+	  public void convert() {
+
+	    ChildUnder3 result =
+	    		MedicalEquipmentConverter.convert(new JourneyBuilder().withEligibility(CHILDBULK).build());
+	    // Detail covered in other tests.
+	    assertThat(result.getBulkyMedicalEquipmentTypeCodes().size()).isEqualTo(2);
+	    assertThat(result.getBulkyMedicalEquipmentTypeCodes()).contains(PUMP);
+	    assertThat(result.getBulkyMedicalEquipmentTypeCodes()).contains(OTHER);
+	    assertThat(result.getOtherMedicalEquipment()).isEqualTo("another medical equipment");
+	  }
+}
