959d61e0a86a8c6db3e90e18788220683e2d1436
Miguel Gil <mgilgar@gmail.com>
2018-11-20 11:03:10 +0000

    [BBB-1034-add-all-local-councils-into-registered-blind-picker] fix regression tests in jenkins.

--- acceptance-tests/env-feature.sh
+++ acceptance-tests/env-feature.sh
@@ -2,4 +2,4 @@
 
 # Override the version with the branch name
 export CA_VERSION=$(cat VERSION-computed)
-
+export RD_VERSION=v0.12.0-rc1-feature_BBB-1034-add-all-local-councils-into-registered-blind-picker
