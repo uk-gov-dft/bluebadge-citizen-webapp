fa6f3f7554883e2d244755113460c2558b54a30d
Miguel Gil <mgilgar@gmail.com>
2018-11-23 15:49:21 +0000

    [BBB-693-select-your-council-signpost-non-beta-users-to-northgate-application-journey] update dependency for acceptance test in jenkins.

--- acceptance-tests/env-feature.sh
+++ acceptance-tests/env-feature.sh
@@ -2,3 +2,4 @@
 
 # Override the version with the branch name
 export CA_VERSION=$(cat VERSION-computed)
+export RD_VERSION=v0.12.0-feature_BBB-693-select-your-council-signpost-non-beta-users-to-northgate-application-journey
