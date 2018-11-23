d76e50a21c4bddde65eba70917fe947ec46d26cc
Miguel Gil <mgilgar@gmail.com>
2018-11-23 15:54:38 +0000

    [BBB-693-select-your-council-signpost-non-beta-users-to-northgate-application-journey] sonar qube comments addressed.

--- src/main/java/uk/gov/dft/bluebadge/webapp/citizen/appbuilder/JourneyToApplicationConverter.java
+++ src/main/java/uk/gov/dft/bluebadge/webapp/citizen/appbuilder/JourneyToApplicationConverter.java
@@ -14,17 +14,14 @@ public class JourneyToApplicationConverter {
   public static Application convert(Journey journey) {
     ExistingBadgeForm existingBadgeForm = journey.getFormForStep(StepDefinition.EXISTING_BADGE);
 
-    Application app =
-        Application.builder()
-            .applicationTypeCode(ApplicationTypeCodeField.NEW)
-            .localAuthorityCode(journey.getLocalAuthority().getShortCode())
-            .paymentTaken(false)
-            .existingBadgeNumber(getExistingBadgeNumber(existingBadgeForm))
-            .party(PartyConverter.convert(journey))
-            .eligibility(EligibilityConverter.convert(journey))
-            .build();
-
-    return app;
+    return Application.builder()
+        .applicationTypeCode(ApplicationTypeCodeField.NEW)
+        .localAuthorityCode(journey.getLocalAuthority().getShortCode())
+        .paymentTaken(false)
+        .existingBadgeNumber(getExistingBadgeNumber(existingBadgeForm))
+        .party(PartyConverter.convert(journey))
+        .eligibility(EligibilityConverter.convert(journey))
+        .build();
   }
 
   static String getExistingBadgeNumber(ExistingBadgeForm existingBadgeForm) {
@@ -34,10 +31,7 @@ public class JourneyToApplicationConverter {
         && existingBadgeForm.getHasExistingBadge()
         && StringUtils.isNotBlank(existingBadgeForm.getBadgeNumber())) {
 
-      String badgeNumber =
-          existingBadgeForm.getBadgeNumber().replaceAll("\\s+", "").substring(0, 6);
-
-      return badgeNumber;
+      return existingBadgeForm.getBadgeNumber().replaceAll("\\s+", "").substring(0, 6);
     }
 
     return null;
