3cd794f1f46b1b86bda25ea374d7f5b219d50af8
Stephen Elliott <stephen@bealine.co.uk>
2018-11-28 10:20:12 +0000

    BBB-918 Update acceptance test dependencies

--- acceptance-tests/env-feature.sh
+++ acceptance-tests/env-feature.sh
@@ -3,4 +3,4 @@
 # Override the version with the branch name
 export CA_VERSION=$(cat VERSION-computed)
 
-export AP_VERSION=v0.10.0-rc1-feature_BBB-919-upload-multiple-docs
+export AP_VERSION=v0.11.0-rc1-feature_BBB-919-upload-multiple-docs
