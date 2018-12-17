13ee1e16b94df8e9448473800b9d16c4aa393a55
Ali Ashik <ali.ashik@valtech.co.uk>
2018-12-17 15:12:46 +0000

    BBB-1074 fixed acceptance tests

--- acceptance-tests/src/test/java/uk/gov/service/bluebadge/test/acceptance/pages/site/AddMobilityAidsPage.java
+++ acceptance-tests/src/test/java/uk/gov/service/bluebadge/test/acceptance/pages/site/AddMobilityAidsPage.java
@@ -10,8 +10,8 @@ public class AddMobilityAidsPage {
   public static final String MOBILITY_AID_ADD_USAGE = "usage";
   public static final String MOBILITY_AID_ADD_CONFIRM_BUTTON = "button.continue";
 
-  public static final String VALIDATION_MESSAGE_FOR_NO_TYPE = "Enter the type of mobility aid";
-  public static final String VALIDATION_MESSAGE_FOR_NO_HOW_USED = "Enter how mobility aid is used";
+  public static final String VALIDATION_MESSAGE_FOR_NO_TYPE = "Enter the type of mobility aid or support";
+  public static final String VALIDATION_MESSAGE_FOR_NO_HOW_USED = "Enter how mobility aid or support is used";
   public static final String VALIDATION_MESSAGE_FOR_NO_HOW_PROVIDED =
-      "Select how this mobility aid was provided";
+      "Select how this mobility aid or support was provided";
 }
