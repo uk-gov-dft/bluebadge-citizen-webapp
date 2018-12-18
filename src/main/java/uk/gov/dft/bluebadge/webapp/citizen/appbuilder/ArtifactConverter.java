571954d04b15070a719a4318aea23987fe7ef86f
Stephen Elliott <stephen@bealine.co.uk>
2018-12-18 12:41:21 +0000

    BBB-945 New artifact type for supporting docs

--- src/main/java/uk/gov/dft/bluebadge/webapp/citizen/appbuilder/ArtifactConverter.java
+++ src/main/java/uk/gov/dft/bluebadge/webapp/citizen/appbuilder/ArtifactConverter.java
@@ -1,30 +1,47 @@
 package uk.gov.dft.bluebadge.webapp.citizen.appbuilder;
 
+import static uk.gov.dft.bluebadge.webapp.citizen.client.applicationmanagement.model.EligibilityCodeField.ARMS;
+import static uk.gov.dft.bluebadge.webapp.citizen.client.applicationmanagement.model.EligibilityCodeField.CHILDBULK;
+import static uk.gov.dft.bluebadge.webapp.citizen.client.applicationmanagement.model.EligibilityCodeField.CHILDVEHIC;
+import static uk.gov.dft.bluebadge.webapp.citizen.client.applicationmanagement.model.EligibilityCodeField.DLA;
 import static uk.gov.dft.bluebadge.webapp.citizen.client.applicationmanagement.model.EligibilityCodeField.PIP;
+import static uk.gov.dft.bluebadge.webapp.citizen.client.applicationmanagement.model.EligibilityCodeField.WALKD;
 
 import java.util.ArrayList;
+import java.util.EnumSet;
 import java.util.List;
 import java.util.stream.Collectors;
 import org.springframework.util.Assert;
 import uk.gov.dft.bluebadge.webapp.citizen.client.applicationmanagement.model.Artifact;
 import uk.gov.dft.bluebadge.webapp.citizen.client.applicationmanagement.model.ArtifactType;
+import uk.gov.dft.bluebadge.webapp.citizen.client.applicationmanagement.model.EligibilityCodeField;
 import uk.gov.dft.bluebadge.webapp.citizen.controllers.journey.StepDefinition;
 import uk.gov.dft.bluebadge.webapp.citizen.model.Journey;
 import uk.gov.dft.bluebadge.webapp.citizen.model.form.ArtifactForm;
 import uk.gov.dft.bluebadge.webapp.citizen.model.form.ProveIdentityForm;
 import uk.gov.dft.bluebadge.webapp.citizen.model.form.ProvidePhotoForm;
 import uk.gov.dft.bluebadge.webapp.citizen.model.form.UploadBenefitForm;
+import uk.gov.dft.bluebadge.webapp.citizen.model.form.UploadSupportingDocumentsForm;
 
 class ArtifactConverter {
+  private static EnumSet<EligibilityCodeField> BENEFIT_UPLOAD_ELIG_TYPES = EnumSet.of(PIP, DLA);
+  private static EnumSet<EligibilityCodeField> SUPPORT_DOCS_ELIG_TYPES =
+      EnumSet.of(WALKD, ARMS, CHILDBULK, CHILDVEHIC);
 
   private ArtifactConverter() {}
 
   static List<Artifact> convert(Journey journey) {
     List<Artifact> result = new ArrayList<>();
 
-    if (PIP == journey.getEligibilityCode()) {
+    if (BENEFIT_UPLOAD_ELIG_TYPES.contains(journey.getEligibilityCode())) {
       UploadBenefitForm uploadBenefitForm = journey.getFormForStep(StepDefinition.UPLOAD_BENEFIT);
       convertArtifact(result, uploadBenefitForm, ArtifactType.PROOF_ELIG);
+    }
+
+    if (SUPPORT_DOCS_ELIG_TYPES.contains(journey.getEligibilityCode())) {
+      UploadSupportingDocumentsForm form =
+          journey.getFormForStep(StepDefinition.UPLOAD_SUPPORTING_DOCUMENTS);
+      convertArtifact(result, form, ArtifactType.SUPPORT_DOCS);
     }
 
     ProveIdentityForm proveIdentityForm = journey.getFormForStep(StepDefinition.PROVE_IDENTITY);
--- src/main/java/uk/gov/dft/bluebadge/webapp/citizen/client/applicationmanagement/model/ArtifactType.java
+++ src/main/java/uk/gov/dft/bluebadge/webapp/citizen/client/applicationmanagement/model/ArtifactType.java
@@ -2,6 +2,7 @@ package uk.gov.dft.bluebadge.webapp.citizen.client.applicationmanagement.model;
 
 public enum ArtifactType {
   PROOF_ELIG,
+  SUPPORT_DOCS,
   PROOF_ADD,
   PROOF_ID,
   PHOTO
