8ee12c0afa37e04ca2973cf6583bc0f844b83857
Robert Smallwood <rob.smallwood@valtech.co.uk>
2019-01-02 15:53:15 +0000

    BBB-942 - reverting more of the PR changes..

--- src/main/java/uk/gov/dft/bluebadge/webapp/citizen/client/applicationmanagement/model/ArtifactType.java
+++ src/main/java/uk/gov/dft/bluebadge/webapp/citizen/client/applicationmanagement/model/ArtifactType.java
@@ -2,7 +2,7 @@ package uk.gov.dft.bluebadge.webapp.citizen.client.applicationmanagement.model;
 
 public enum ArtifactType {
   PROOF_ELIG,
-  SUPPORT_DOC,
+  SUPPORT_DOCS,
   PROOF_ADD,
   PROOF_ID,
   PHOTO
