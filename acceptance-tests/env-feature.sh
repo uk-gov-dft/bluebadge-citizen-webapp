d0ddd748ae565ce8af0300a4cb1dff4102125d83
Stephen Elliott <stephen@bealine.co.uk>
2019-01-24 08:36:03 +0000

    BBB-777 Update acceptance test dependencies

--- acceptance-tests/env-feature.sh
+++ acceptance-tests/env-feature.sh
@@ -2,3 +2,6 @@
 
 # Override the version with the branch name
 export CA_VERSION=$(cat VERSION-computed)
+
+export AP_VERSION=v0.13.0-feature_BBB-777-la-notify-app-submission
+export MG_VERSION=v0.9.0-feature_BBB-777-la-notify
