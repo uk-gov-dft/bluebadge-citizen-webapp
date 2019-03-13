5fec361cde9360bf03abb87e18e593f45dfbe1f6
Stephen Elliott <stephen@bealine.co.uk>
2019-03-13 11:31:38 +0000

    BBB-1280 Give the exception a message when created with a common response.

--- src/main/java/uk/gov/dft/bluebadge/webapp/citizen/client/common/ClientApiException.java
+++ src/main/java/uk/gov/dft/bluebadge/webapp/citizen/client/common/ClientApiException.java
@@ -1,15 +1,29 @@
 package uk.gov.dft.bluebadge.webapp.citizen.client.common;
 
+import java.util.stream.Collectors;
 import uk.gov.dft.bluebadge.common.api.model.CommonResponse;
 
 public class ClientApiException extends RuntimeException {
   final transient CommonResponse commonResponse;
 
   public ClientApiException(CommonResponse commonResponse) {
+    super(extractMessage(commonResponse));
     this.commonResponse = commonResponse;
   }
 
   public CommonResponse getCommonResponse() {
     return commonResponse;
+  }
+
+  private static String extractMessage(CommonResponse cr){
+    if(null == cr || null == cr.getError() || null == cr.getError().getErrors()){
+      return null;
+    }
+
+    String result = null == cr.getError().getMessage() ? "" : cr.getError().getMessage() + ": ";
+    result = cr.getError().getErrors().stream()
+        .map(e -> e.getMessage())
+        .collect(Collectors.joining(", ", result, ""));
+    return result;
   }
 }
