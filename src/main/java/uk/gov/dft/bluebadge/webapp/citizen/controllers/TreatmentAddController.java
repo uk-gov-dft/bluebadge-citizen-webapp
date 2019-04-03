0b3d56656afb2d051185ced197bbda31e5bef861
Rob Pataki <robert.pataki@valtech.com>
2019-04-03 16:11:53 +0100

    BBB-1238 Update UI

--- src/main/java/uk/gov/dft/bluebadge/webapp/citizen/controllers/TreatmentAddController.java
+++ src/main/java/uk/gov/dft/bluebadge/webapp/citizen/controllers/TreatmentAddController.java
@@ -5,6 +5,7 @@ import static uk.gov.dft.bluebadge.webapp.citizen.model.Journey.FORM_REQUEST;
 import static uk.gov.dft.bluebadge.webapp.citizen.model.Journey.JOURNEY_SESSION_KEY;
 
 import java.util.ArrayList;
+import java.util.List;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -19,6 +20,8 @@ import uk.gov.dft.bluebadge.webapp.citizen.controllers.journey.Mappings;
 import uk.gov.dft.bluebadge.webapp.citizen.controllers.journey.RouteMaster;
 import uk.gov.dft.bluebadge.webapp.citizen.controllers.journey.StepDefinition;
 import uk.gov.dft.bluebadge.webapp.citizen.model.Journey;
+import uk.gov.dft.bluebadge.webapp.citizen.model.RadioOption;
+import uk.gov.dft.bluebadge.webapp.citizen.model.RadioOptionsGroup;
 import uk.gov.dft.bluebadge.webapp.citizen.model.form.TreatmentAddForm;
 import uk.gov.dft.bluebadge.webapp.citizen.model.form.TreatmentListForm;
 
@@ -28,6 +31,12 @@ public class TreatmentAddController implements StepController {
 
   private RouteMaster routeMaster;
   private static final String TEMPLATE = "treatment-add";
+
+  private enum WHEN_TYPE_CODE {
+    PAST,
+    ONGOING,
+    FUTURE;
+  }
 
   @Autowired
   TreatmentAddController(RouteMaster routeMaster) {
@@ -74,6 +83,20 @@ public class TreatmentAddController implements StepController {
 
     return "redirect:" + Mappings.URL_TREATMENT_LIST;
   }
+
+    @ModelAttribute("whenTypeCodes")
+    public RadioOptionsGroup whenTypeCodes(@ModelAttribute(JOURNEY_SESSION_KEY) Journey journey) {
+      if (!routeMaster.isValidState(getStepDefinition(), journey)) {
+        return null;
+      }
+
+      List<RadioOption> options = new ArrayList<>();
+      options.add(new RadioOption(WHEN_TYPE_CODE.PAST, "treatment.add.when.past.title"));
+      options.add(new RadioOption(WHEN_TYPE_CODE.ONGOING, "treatment.add.when.ongoing.title"));
+      options.add(new RadioOption(WHEN_TYPE_CODE.FUTURE, "treatment.add.when.future.title"));
+
+      return new RadioOptionsGroup("treatment.add.when.title", options);
+    }
 
   @Override
   public StepDefinition getStepDefinition() {
--- src/main/java/uk/gov/dft/bluebadge/webapp/citizen/model/form/TreatmentAddForm.java
+++ src/main/java/uk/gov/dft/bluebadge/webapp/citizen/model/form/TreatmentAddForm.java
@@ -5,21 +5,31 @@ import java.io.Serializable;
 import java.util.List;
 import java.util.UUID;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import uk.gov.dft.bluebadge.webapp.citizen.client.applicationmanagement.model.WalkingDifficultyTypeCodeField;
 
 @Data
 @EqualsAndHashCode
 public class TreatmentAddForm implements BaseForm, Serializable {
+  @NotNull(message = "{NotNull.treatment.whenTypeCodeField}")
+  private String whenTypeCodeField;
 
   @Size(max = 100)
   @NotBlank
   private String treatmentDescription;
 
   @Size(max = 100)
-  @NotBlank
   private String treatmentWhen;
+
+  @Size(max = 100)
+  private String treatmentDetailField;
+
+  @Size(max = 100)
+  private String treatmentOptionalDetailField;
 
   private String id = UUID.randomUUID().toString();
 
