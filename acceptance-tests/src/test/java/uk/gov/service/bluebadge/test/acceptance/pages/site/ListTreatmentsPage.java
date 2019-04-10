27d5409b6c43f9cc8714789e82b98701eade89c0
Rob Pataki <robert.pataki@valtech.com>
2019-04-10 12:11:44 +0100

    BBB-1341 Update more failing test

--- acceptance-tests/src/test/java/uk/gov/service/bluebadge/test/acceptance/pages/site/ListTreatmentsPage.java
+++ acceptance-tests/src/test/java/uk/gov/service/bluebadge/test/acceptance/pages/site/ListTreatmentsPage.java
@@ -5,7 +5,7 @@ public class ListTreatmentsPage {
   public static final String PAGE_URL = "/list-treatments";
   public static final String PAGE_TITLE = "Has your condition required any treatments?";
   public static final String PAGE_TITLE_SOMEONE_ELSE =
-      "Has your condition required any treatments?";
+      "Has their condition required any treatments?";
 
   public static final String HEADER = "Has your condition required any treatments?";
   public static final String HEADER_SOMEONE_ELSE = "Has their condition required any treatments?";
@@ -14,7 +14,7 @@ public class ListTreatmentsPage {
   public static final String TREATMENT_NO_OPTION = "hasTreatmentno";
 
   public static final String VALIDATION_MESSAGE_FOR_NO_OPTION =
-      "Select whether treatments have been received";
+      "Select whether condition has required treatments";
 
   public static final String ADD_FIRST_TREATMENT = "firstaddtreatmentlink";
 }
