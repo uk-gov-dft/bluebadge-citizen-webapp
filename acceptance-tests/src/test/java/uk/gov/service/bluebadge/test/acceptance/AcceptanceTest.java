1dcc4bb2428a7a2281aac25e56cc1d93c12f91c1
smahavithana <smahavithana@gmail.com>
2019-05-02 15:25:27 +0100

    Fixed reporting error

--- acceptance-tests/src/test/java/uk/gov/service/bluebadge/test/acceptance/AcceptanceTest.java
+++ acceptance-tests/src/test/java/uk/gov/service/bluebadge/test/acceptance/AcceptanceTest.java
@@ -7,12 +7,7 @@ import org.junit.runner.RunWith;
 
 /** Entry point required by {@code cucumber-jvm} to discover and run tests. */
 @RunWith(Cucumber.class)
-@CucumberOptions(
-  features = "src/test/resources/features/site/testSuite1/",
-  plugin = {"com.cucumber.listener.ExtentCucumberFormatter:output/report.html"}
-)
+@CucumberOptions(features = "src/test/resources/features/site/testSuite1/")
 public class AcceptanceTest {
   // no-op, config class only
-  @AfterClass
-  public static void teardown() {}
 }
