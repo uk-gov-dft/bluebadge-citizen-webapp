fcf00ac2f57495e95750689e2938d2d6fdd513f1
smahavithana <smahavithana@gmail.com>
2019-05-07 06:23:16 +0100

    Corrected Runner Files

--- acceptance-tests/src/test/java/uk/gov/service/bluebadge/test/acceptance/AcceptanceTest5.java
+++ acceptance-tests/src/test/java/uk/gov/service/bluebadge/test/acceptance/AcceptanceTest5.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 
 /** Entry point required by {@code cucumber-jvm} to discover and run tests. */
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/features/site/testSuite4/")
+@CucumberOptions(features = "src/test/resources/features/site/testSuite5/")
 public class AcceptanceTest5 {
   // no-op, config class only
 }
--- acceptance-tests/src/test/java/uk/gov/service/bluebadge/test/acceptance/AcceptanceTest6.java
+++ acceptance-tests/src/test/java/uk/gov/service/bluebadge/test/acceptance/AcceptanceTest6.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 
 /** Entry point required by {@code cucumber-jvm} to discover and run tests. */
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/features/site/testSuite4/")
+@CucumberOptions(features = "src/test/resources/features/site/testSuite6/")
 public class AcceptanceTest6 {
   // no-op, config class only
 }
